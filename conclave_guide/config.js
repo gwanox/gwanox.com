1bd4dcad763a9f39a2d9f71efe5d42354b7ea8f7
gwan <33358847+gwanox@users.noreply.github.com>
2025-08-05 21:51:06 +0200

    Update config.js

--- conclave_guide/config.js
+++ conclave_guide/config.js
@@ -2,9 +2,6 @@ window.CONFIG = {
   // Appears above the search box and in the <title>
   TITLE   : 'Conclave Guide',
 
-  // Any png / ico / svg file you like
-  FAVICON : 'logo.webp',
-
   // Raw Markdown bundle (one file, many pages)
   MD : 'https://hackmd.io/@conc/guide/download',
 
